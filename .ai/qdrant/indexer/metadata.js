0f699b6e8d1fad0f2f068b73833a617d8415dcd0
Andrew Clinton <andrew.clinton@sonar.software>
2026-05-12 09:53:55 -0400

    better layer segmentation
    bring qdrant in to alignment and parity with cocoindex layers

--- .ai/qdrant/indexer/metadata.js
+++ .ai/qdrant/indexer/metadata.js
@@ -1,10 +1,20 @@
 export function detectLaravelLayer(relPath) {
-  if (/\/ui\/app\//i.test(relPath)) return 'frontend';
-  if (/\/Services\//i.test(relPath)) return 'service';
-  if (/\/Controllers\//i.test(relPath)) return 'controller';
-  if (/\/Jobs\//i.test(relPath)) return 'job';
-  if (/\/Requests\//i.test(relPath)) return 'request';
-  if (/\/GraphQL\/|\/Mutations\//i.test(relPath)) return 'graphql';
+  const normalizedPath = relPath.replace(/\\/g, '/');
+
+  if (/\/ui\/app\//i.test(normalizedPath)) return 'frontend';
+  if (/\/Services\//i.test(normalizedPath)) return 'service';
+  if (/\/Http\/Controllers\//i.test(normalizedPath)) return 'controller';
+  if (/\/Jobs\//i.test(normalizedPath)) return 'job';
+  if (/\/Http\/Requests\//i.test(normalizedPath)) return 'request';
+  if (/\/GraphQL\/|\/Mutations\//i.test(normalizedPath)) return 'graphql';
+  if (/\/Console\//i.test(normalizedPath)) return 'console';
+  if (/\/Events\//i.test(normalizedPath)) return 'event';
+  if (/\/Listeners\//i.test(normalizedPath)) return 'listener';
+  if (/\/Notifications\//i.test(normalizedPath)) return 'notification';
+  if (/\/Observers\//i.test(normalizedPath)) return 'observer';
+  if (/\/Providers\//i.test(normalizedPath)) return 'provider';
+  if (/\/Models\//i.test(normalizedPath)) return 'model';
+  if (/\/app\/[^/]+\.php$/i.test(normalizedPath)) return 'model';
   return 'other';
 }
 
